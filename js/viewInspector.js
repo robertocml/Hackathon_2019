f5a828595e5f2bf2139e0eb868d9b7df235439b7
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-12-07 22:27:58 -0600

    view edit y add updates

--- js/viewInspector.js
+++ js/viewInspector.js
@@ -4,8 +4,19 @@ let nombre = 'Armando'
 
 
 $('#btn_palomita').click(function(){
+  swal({
+    title : 'Buscando usuario...',
+    timer : 1000,
+    buttons: false
+  }).then(() => {$('#form_view').submit(() => {
+    
+    //hacer el submit y no se hace reload de la pagina
+    return false;
+  
+  })}).then( () => (
+  
     swal({
-        title: "¿Cobrar $" + cantidad + " a " + nombre +"?",
+        title: "¿Cobrar $" + cantidad + " a " + $("#oferente").val() +"?",
         icon: "warning",
         buttons: true,
         dangerMode: true,
@@ -14,24 +25,33 @@ $('#btn_palomita').click(function(){
       .then((willDelete) => {
         if (willDelete) {
           swal( {
-            title: "Se ha cobrado con exito a " + nombre,
+            title: "Se ha cobrado con exito a " + $("#oferente").val(),
             icon: "success",
             closeOnClickOutside: false,
           }).then(() => {
               //llamada de generacion a pdf
             swal('se genera el pdf... aqui va la accion');
+            
         })         
           
         } else {
           swal({
-              title: "No se ha cobrado a " + nombre,
+              title: "No se ha cobrado a " + $("#oferente").val(),
               icon: "error",
               closeOnClickOutside: false
 
           });
         }
-      });
+      }))); 
 
+})
+
+$('#btn_edita').click(() => {
+  window.location.assign("EditOferente.html");
+})
+
+$('#btn_add').click(() => {
+  window.location.assign("AddOferente.html");
 })
 
 
